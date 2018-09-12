a1e84ef2cbe28c68a7f60fce12eaa0e7effc02a0
Emdad Hossain <emdad.hossain321@gmail.com>
2018-09-12 15:56:53 +0600

    added userdefined

---
+++ userdefinedfunction.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emdad
+ * Date: 9/12/2018
+ * Time: 3:46 PM
+ */
+
+$x = 100;
+// declaring the specific user defined function
+function newCalc($x){
+    $newnumber = $x * 0.75;
+
+    echo "Here is 75% of what you wrote: ".$newnumber;
+}
+
+newCalc($x);
+?>
