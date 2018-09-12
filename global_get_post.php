2248c32646a5f16ffcaf67746b3db009bba32a47
emdad <emdad.hossain321@gmail.com>
2018-09-12 21:51:43 +0600

    added some code for global variable for get and post

---
+++ global_get_post.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<?php
+echo  "<br>".$_POST['name'];
+echo  "<br>".$_POST['name'];
+echo  "<br>".$_POST['name'];
+?>
+
+<html>
+
+<form method="POST">
+    <input type="hidden" name="name" value = "emdssad">
+    <button type = "submit">press me</button>
+</form>
+
+
+</html>
