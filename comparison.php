5181374c45c5779d9ad8be82906d368345b3a5b6
Emdad Hossain <emdad.hossain321@gmail.com>
2018-09-12 13:57:45 +0600

    added comparison operator

---
+++ comparison.php
@@ -0,0 +1,14 @@
+
+<?php
+
+$x = 5;
+$y = "5";
+
+if($x == $y){
+echo "true!";
+}
+else{
+ echo "false!";
+}
+
+?>
--- index.php
+++ index.php
@@ -15,14 +15,18 @@
 <?php
 //loaded faster
 echo "hello world ";
-print 12;
+print 12 ;
 
 // how to create a variable in php
 // get the method for SUBMIT button using the name attribute as identifier
 $name = $_GET['user'];
 
-// here is the dot notation for concating string
-echo "\n " . $name . " is a handsome fellow";
+
+
+echo $name. "my mane";
+
+$name_arrays = array("emdad","hossain");
+echo $name_arrays[1];
 
 ?>
 </html>
